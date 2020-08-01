a9a30334abb12b97f0e0947b91600916970a6bdf
Merlin Carson <mpc6@.pdx.edu>
2020-08-01 16:11:12 -0700

    sequential matrix multiplication

---
+++ unifiedMem/main.cpp
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <random>
+
+
+void matmul(float * M, float * N, float * P, int Mrows, int Mcols, int Nrows, int Ncols ){
+
+  // iterate over output dimensions
+  for(int i = 0; i < Mrows; ++i){
+    for(int j = 0; j < Ncols; ++j){
+      float val = 0.0;
+      for(int k = 0; k < Mcols; ++k){
+        val += M[i*Mcols+k] * N[k*Ncols+j];
+      }
+      P[i*Ncols+j] = val;
+    }
+  }
+
+}
+
+void printMatrix(float * matrix, int rows, int cols){
+
+  for(int i = 0; i < rows; ++i){
+    for(int j = 0; j < cols; ++j){
+      printf("%f ", matrix[i*cols+j]);
+    }
+    printf("\n");
+  }
+
+}
+
+void fillMatrix(float * matrix, int rows, int cols){
+  int maxVal = 4;
+
+  for(int i = 0; i < rows; ++i){
+    for(int j = 0; j < cols; ++j){
+      matrix[i*cols+j] = rand() % maxVal;
+    }
+  }
+
+}
+
+int main(void){
+
+  int Mrows = 4;
+  int Mcols = 4;
+  int Nrows = Mcols;
+  int Ncols = 4;
+
+  float * M = (float*)malloc(Mrows*Mcols*sizeof(float));
+  float * N = (float*)malloc(Mrows*Mcols*sizeof(float));
+
+  float * P = (float*)calloc(Mrows*Ncols, sizeof(float));
+
+  fillMatrix(M, Mrows, Mcols);
+  printf("Matrix M: \n");
+  printMatrix(M, Mrows, Mcols);
+
+  fillMatrix(N, Nrows, Ncols);
+  printf("Matrix N: \n");
+  printMatrix(N, Nrows, Ncols);
+  
+  matmul(M, N, P, Mrows, Mcols, Nrows, Ncols);
+  printf("Matrix M*N: \n");
+  printMatrix(P, Mcols, Nrows);
+}
